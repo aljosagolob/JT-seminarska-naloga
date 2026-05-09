5bd8d0ff4690033b918ac4716d2eec2abdcac470
NevihtniOblak <nejcxfras@gmail.com>
2026-05-09 16:30:24 +0200

    added preprocessing module and optimization script for preprocessing parameters

--- model.py
+++ model.py
@@ -14,15 +14,15 @@ pipeline = Pipeline.from_pretrained(
 )
 
 # send pipeline to GPU (when available)
-pipeline.to(torch.device("cuda"))
+pipeline.to(torch.device("cuda" if torch.cuda.is_available() else "cpu")) # type: ignore
 
 # apply pretrained pipeline (with optional progress hook)
 with ProgressHook() as hook:
-    output = pipeline("aepyx.wav", hook=hook) 
+    output = pipeline("aepyx.wav", hook=hook) # type: ignore
 
 # with open("diarization.txt", "w") as f:
 #   for turn, speaker in output.speaker_diarization:
-#        f.write(f"{speaker} speaks between t={turn.start:.3f}s and t={turn.end:.3f}s\n") 
+#        f.write(f"{speaker} speaks between t={turn.start:.3f}s and t={turn.end:.3f}s\n")
 
 with open("output.rttm", "w") as f:
     output.speaker_diarization.write_rttm(f)
